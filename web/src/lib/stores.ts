fe82ec730e358a23390772e9e31c3d85b20493c4
Matt Davis <mattdavis90@googlemail.com>
2024-10-13 13:17:39 +0100

    feat: add dark-mode

--- web/src/lib/stores.ts
+++ web/src/lib/stores.ts
@@ -5,3 +5,7 @@ import { writable } from "svelte/store";
 
 export const sysInfo = writable<SystemInfo | undefined>(undefined);
 export const notifications = writable<Notification>(undefined);
+
+export const darkMode = writable<boolean | null>(
+  JSON.parse(localStorage.getItem("darkMode") || "null"),
+);
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,16 +1,55 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { afterNavigate } from "$app/navigation";
   import Navbar from "$lib/components/Navbar.svelte";
   import Notifications from "$lib/components/Notifications.svelte";
   import Sidebar from "$lib/components/Sidebar.svelte";
+  import { darkMode } from "$lib/stores";
 
   import "../app.scss";
 
   let { children } = $props();
   let drawer = $state(false);
+  let prefersDarkMode = $state(false);
 
   afterNavigate(() => {
     drawer = false;
+  });
+
+  onMount(() => {
+    // Watch for change of user preferences
+    if (window.matchMedia) {
+      const wm = window.matchMedia("(prefers-color-scheme: dark)");
+      prefersDarkMode = wm.matches;
+      wm.onchange = (v) => {
+        prefersDarkMode = v.matches;
+      };
+    }
+
+    // Watch for other tabs changing the local storage
+    const updateDarkMode = (ev: StorageEvent) => {
+      if (ev.key == "darkMode") {
+        $darkMode = JSON.parse(ev.newValue || "null");
+      }
+    };
+    window.addEventListener("storage", updateDarkMode);
+
+    return () => {
+      window.removeEventListener("storage", updateDarkMode);
+    };
+  });
+
+  $effect(() => {
+    let theme: string;
+
+    if ($darkMode === null) {
+      theme = prefersDarkMode ? "dark" : "light";
+    } else {
+      theme = $darkMode ? "dark" : "light";
+    }
+
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("darkMode", JSON.stringify($darkMode));
   });
 </script>
 
