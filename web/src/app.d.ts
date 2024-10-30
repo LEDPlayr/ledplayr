5fc519368ec3cfce5137ab168adc9b7f70a979c3
Matt Davis <mattdavis90@googlemail.com>
2024-10-30 22:00:58 +0000

    chore: update deps

--- web/src/app.d.ts
+++ web/src/app.d.ts
@@ -1,24 +1,3 @@
-/* Fix for svelte-5 until
- * https://github.com/unplugin/unplugin-icons/blob/main/types/svelte.d.ts
- * is updated in
- * https://github.com/unplugin/unplugin-icons/pull/381
- */
-declare module "virtual:icons/*" {
-  import type { Component } from "svelte";
-  import type { SvelteHTMLElements } from "svelte/elements";
-
-  const component: Component<SvelteHTMLElements["svg"]>;
-
-  export default component;
-}
-
-declare module "~icons/*" {
-  import type { Component } from "svelte";
-  import type { SvelteHTMLElements } from "svelte/elements";
-
-  const component: Component<SvelteHTMLElements["svg"]>;
-
-  export default component;
-}
+import "unplugin-icons/types/svelte.d.ts";
 
 export {};
